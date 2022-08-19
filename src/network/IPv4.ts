09cf0a410f7af37abef9bc7b30c9cbfbe2e7d5df
DeveloperOscar <161139@unamba.edu.pe>
2022-08-19 13:03:38 -0500

    Iniciando Proyecto

---
+++ src/network/IPv4.ts
@@ -0,0 +1,24 @@
+class IPv4 {
+  private _groups: number[] = []; //notacion decimal
+  public static readonly REGEXP = /^([1-5]{3})([0-5]{3})([0-5]{3})([0-5]{3})$/;
+
+  constructor(ipStr: string) {
+		this.ipStr = ipStr;
+  }
+
+	public set ipStr(str : string){
+		const groups = str.match(IPv4.REGEXP);
+		if( !groups ) throw new Error("Invalid IP");
+		const [_, ...captures] = groups;
+		const groupsMatch = captures.map(capture => Number(capture));
+		if(groupsMatch.every(group => (group >= 0 && group <= 255)))
+			this._groups = groupsMatch;
+		else throw new Error("Invalid IP");
+	}
+
+	public get ipStr(){
+		return this._groups.join(".");
+	}
+}
+
+export default IPv4;
