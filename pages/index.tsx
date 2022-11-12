158b49e08f6d2084ec283d1a58cdf0a7781ca214
DeveloperOscar <161139@unamba.edu.pe>
2022-11-12 11:33:54 -0500

    add google site verification meta tag

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,15 +2,16 @@ import AppIpInfo from "components/organism/AppIpInfo";
 import type { NextPage } from "next";
 import Head from "next/head";
 
- const Home: NextPage = () => {
+const Home: NextPage = () => {
   return (
     <div className="h-auto">
       <Head>
         <title>IP INFO</title>
         <meta name="description" content="Proyecto de Redes" />
+        <meta name="google-site-verification" content="03oTmK_RvtK-xs1VCqA7-EySl23tUJb2i7eoXx9tqzU" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <AppIpInfo/>
+      <AppIpInfo />
     </div>
   );
 };
